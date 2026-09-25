574b4a64b1c43c3aa1dc0b8214c449943f1ba91b
arya237 <naderiarya20@gmail.com>
2026-09-25 19:08:32 +0330

    feat: implement disk infrastructure

--- cmd/Gyscope/main.go
+++ cmd/Gyscope/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	applicationcpu "Gyscope/internal/application/cpu"
+	applicationdisk "Gyscope/internal/application/disk"
 	applicationmemory "Gyscope/internal/application/memory"
 	"Gyscope/internal/delivery/tui"
 	"fmt"
 
 	Linuxcpu "Gyscope/internal/infrastructure/cpu/linux"
+	linuxdisk "Gyscope/internal/infrastructure/disk/linux"
 	Linuxmemory "Gyscope/internal/infrastructure/memory/linux"
 
 	tea "charm.land/bubbletea/v2"
@@ -18,11 +20,13 @@ func main() {
 
 	cpuDataSource := Linuxcpu.NewLinuxCPUDataSource()
 	memoryDataSource := Linuxmemory.NewLinuxMemoryDataSoruce()
+	diskDatasource := linuxdisk.NewLinuxDiskDataSource()
 
 	cpuUsecase := applicationcpu.NewCpuUseCase(cpuDataSource)
 	memoryUsecase := applicationmemory.NewMemoryUseCase(memoryDataSource)
+	diskUsecase := applicationdisk.NewDiskUseCase(diskDatasource)
 
-	model := tui.NewModel(cpuUsecase, memoryUsecase)
+	model := tui.NewModel(cpuUsecase, memoryUsecase, diskUsecase)
 
 	program := tea.NewProgram(model)
 
--- internal/delivery/tui/model.go
+++ internal/delivery/tui/model.go
@@ -2,6 +2,7 @@ package tui
 
 import (
 	"Gyscope/internal/domain/cpu"
+	"Gyscope/internal/domain/disk"
 	"Gyscope/internal/domain/memory"
 
 	tea "charm.land/bubbletea/v2"
@@ -10,18 +11,22 @@ import (
 type Model struct {
 	cpuReader    CPUReader
 	memoryReader MemoryReader
+	diskReader   DiskReader
 	cpu          cpu.CPU
 	memory       memory.Memory
+	disk         disk.Disk
 	memoryErr    error
 	cpuErr       error
+	diskErr      error
 	width        int
 	height       int
 }
 
-func NewModel(cpuReader CPUReader, memoryReader MemoryReader) Model {
+func NewModel(cpuReader CPUReader, memoryReader MemoryReader, diskReader DiskReader) Model {
 	return Model{
 		cpuReader:    cpuReader,
 		memoryReader: memoryReader,
+		diskReader:   diskReader,
 	}
 }
 
